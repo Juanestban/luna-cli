9cfb9815d83b958584d420d57943343af640a4ed
Juan Esteban <juanestban000@gmail.com>
2023-12-16 21:01:31 -0500

    fix: error to try read config file using dynamic import, replaced for require

--- src/utils/getOptions.ts
+++ src/utils/getOptions.ts
@@ -5,22 +5,17 @@ import { getRootPath } from './path';
 import { LunConfig } from '../models';
 
 export const getOptions = () => {
-  return new Promise<LunConfig>(async (resolve, reject) => {
-    try {
-      const searchingFile = extsConfigFile.map((ext) => {
-        const configPath = getRootPath(`lun.config.${ext}`);
-        return fs.existsSync(configPath) ? configPath : false;
-      });
-      if (searchingFile.some((cond) => typeof cond === 'string')) {
-        const configPath = searchingFile.find(
-          (cond) => typeof cond === 'string',
-        ) as string;
-        const options = (await import(configPath)).default as LunConfig;
+  return new Promise<LunConfig>(async (resolve) => {
+    const searchingFile = extsConfigFile.map((ext) => {
+      const configPath = getRootPath(`lun.config.${ext}`);
+      return fs.existsSync(configPath) ? configPath : false;
+    });
+    if (searchingFile.some((cond) => typeof cond === 'string')) {
+      const configPath = searchingFile.find((cond) => typeof cond === 'string') as string;
+      const options = require(configPath) as LunConfig;
+      console.log(options);
 
-        resolve(options);
-      } else resolve(defaultConfig);
-    } catch {
-      reject(defaultConfig);
-    }
+      resolve(options);
+    } else resolve(defaultConfig);
   });
 };
